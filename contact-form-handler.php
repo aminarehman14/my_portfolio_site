4eb77f9a284938a6b692bd3c39119e63fa2301e0
aminarehman14 <46981456+aminarehman14@users.noreply.github.com>
2019-03-13 00:11:01 -0500

    php syntax

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,24 +1,18 @@
 <?php
+
+if(isset($_POST['submit'])) {
 $name = $_POST['name'];
 $visitor_email = $POST['email'];
 $message = $_POST['message'];
 
-$email_from = 'amina_rehman@outlook.com';
-
-$email_subject = 'New Form Submission';
-
-$email_body = "User Name: $name.\n".
-              "User Email: $visitor_email.\n";
-			  "User Message: $message.n\";
-
 $to= 'amina_rehman@outlook.com';
+$headers = "From: ".$visitor_email;
+$txt = "Message from ".$name.".\n\n".$message;
 
-$headers = "From: $email_from\r\n";
-$headers = "Reply-To: $visitor_email" \r\n";
-
-mail($to,$email_subject,$email_body,$headers);
-
+mail($to, $email_subject, $email_body, $headers);
 header("Location: contact.html");
+
+}
 
 
 ?>
